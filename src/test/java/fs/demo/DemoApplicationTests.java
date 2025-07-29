ef288dee5bbab86db95099d3e16c7b80cb97417f
shipeixi <104666667+shipeixi@users.noreply.github.com>
2025-07-29 16:58:28 +0800

    init

---
+++ src/test/java/fs/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
